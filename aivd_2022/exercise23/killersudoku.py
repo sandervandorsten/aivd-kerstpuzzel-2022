146535af2d91f299318742d730a2a184ab08c4b8
Sander van Dorsten <sandervandorsten@gmail.com>
2022-12-31 01:21:26 +0100

    exersice 23: finished exactsum hack, trying for allmost all cages.

--- aivd_2022/exercise23/killersudoku.py
+++ aivd_2022/exercise23/killersudoku.py
@@ -1,7 +1,108 @@
 from dataclasses import dataclass
 from itertools import product
+import datetime as dt
+from pathlib import Path
 
-from constraint import Problem, AllDifferentConstraint, ExactSumConstraint
+from constraint import Problem, AllDifferentConstraint, ExactSumConstraint, Constraint
+
+pieces = {
+    "WK": 100,
+    "WQ": 10,
+    "WI": 6,  # Nightrider
+    "WR": 5,
+    "WB": 3.01,
+    "WN": 3.02,
+    "WG": 2,  # Grasshopper (inverted queen)
+    "WP": 1,
+    "BK": -100,
+    "BQ": -10,
+    "BI": -6,  # Nightrider
+    "BR": -5,
+    "BB": -3.01,
+    "BN": -3.02,
+    "BG": -2,  # Grasshopper (inverted queen)
+    "BP": -1,
+}
+pieces_inv = {v: k for k, v in pieces.items()}
+columns = list("abcdefgh")
+rows = list(map(str, range(1, 9)))
+
+
+class RoundedSumConstraint(ExactSumConstraint):
+    """
+    Constraint enforcing that values of given variables sum roughly
+    to a given amount
+    """
+
+    def preProcess(self, variables, domains, constraints, vconstraints):
+        Constraint.preProcess(self, variables, domains, constraints, vconstraints)
+        multipliers = self._multipliers
+        exactsum = self._exactsum
+        if multipliers:
+            raise NotImplementedError()
+            # for variable, multiplier in zip(variables, multipliers):
+            #     domain = domains[variable]
+            #     for value in domain[:]:
+            #         if round(value) * multiplier > exactsum:
+            #             domain.remove(value)
+        else:
+            pass
+            # Don't prune values
+            # for variable in variables:
+            #     domain = domains[variable]
+            #     for value in domain[:]:
+            #         if round(value) > exactsum:
+            #             domain.remove(value)
+
+    def __call__(self, variables, domains, assignments, forwardcheck=False):
+        multipliers = self._multipliers
+        exactsum = self._exactsum
+        sum = 0
+        missing = False
+        if multipliers:
+            raise NotImplementedError
+            # for variable, multiplier in zip(variables, multipliers):
+            #     if variable in assignments:
+            #         sum += assignments[variable] * multiplier
+            #     else:
+            #         missing = True
+            # if type(sum) is float:
+            #     sum = round(sum)
+            # if sum > exactsum:
+            #     return False
+            # if forwardcheck and missing:
+            #     for variable, multiplier in zip(variables, multipliers):
+            #         if variable not in assignments:
+            #             domain = domains[variable]
+            #             for value in domain[:]:
+            #                 if sum + value * multiplier > exactsum:
+            #                     domain.hideValue(value)
+            #             if not domain:
+            #                 return False
+        else:
+            # create sum based on variable assignments
+            for variable in variables:
+                if variable in assignments:
+                    sum += assignments[variable]
+                else:
+                    missing = True
+            sum = round(sum)
+            # DONT EXCLUDE VALUES IF LARGER THAN exactsum because we are above and below 0
+            # if sum > exactsum:
+            #     return False
+            # if forwardcheck and missing:
+            #     for variable in variables:
+            #         if variable not in assignments:
+            #             domain = domains[variable]
+            #             for value in domain[:]:
+            #                 if sum + value > exactsum:
+            #                     domain.hideValue(value)
+            #             if not domain:
+            #                 return False
+        if missing:
+            return True
+        else:
+            return sum == exactsum
 
 
 @dataclass
@@ -109,61 +210,42 @@ class Variable(object):
 
 
 def main():
-    pieces = {
-        "WK": 100,
-        "WQ": 10,
-        "WI": 6,  # Nightrider
-        "WR": 5,
-        "WB": 3,
-        "WN": 3,
-        "WG": 2,  # Grasshopper (inverted queen)
-        "WP": 1,
-        "BK": -100,
-        "BQ": -10,
-        "BI": -6,  # Nightrider
-        "BR": -5,
-        "BB": -3,
-        "BN": -3,
-        "BG": -2,  # Grasshopper (inverted queen)
-        "BP": -1,
-    }
-
-    pieces_vars = {
-        "WK": Variable(name="WK", value=100),
-        "WQ": Variable(name="WQ", value=10),
-        "WI": Variable(name="WI", value=6),  # Nightrider
-        "WR": Variable(name="WR", value=5),
-        "WB": Variable(name="WB", value=3),
-        "WN": Variable(name="WN", value=3),
-        "WG": Variable(name="WG", value=2),  # Grasshopper (inverted queen)
-        "WP": Variable(name="WP", value=1),
-        "BK": Variable(name="BK", value=-100),
-        "BQ": Variable(name="BQ", value=-10),
-        "BI": Variable(name="BI", value=-6),  # Nightrider
-        "BR": Variable(name="BR", value=-5),
-        "BB": Variable(name="BB", value=-3),
-        "BN": Variable(name="BN", value=-3),
-        "BG": Variable(name="BG", value=-2),  # Grasshopper (inverted queen)
-        "BP": Variable(name="BP", value=-1),
-    }
+    start_time = dt.datetime.now()
+    # pieces_vars = {
+    #     "WK": Variable(name="WK", value=100),
+    #     "WQ": Variable(name="WQ", value=10),
+    #     "WI": Variable(name="WI", value=6),  # Nightrider
+    #     "WR": Variable(name="WR", value=5),
+    #     "WB": Variable(name="WB", value=3),
+    #     "WN": Variable(name="WN", value=3),
+    #     "WG": Variable(name="WG", value=2),  # Grasshopper (inverted queen)
+    #     "WP": Variable(name="WP", value=1),
+    #     "BK": Variable(name="BK", value=-100),
+    #     "BQ": Variable(name="BQ", value=-10),
+    #     "BI": Variable(name="BI", value=-6),  # Nightrider
+    #     "BR": Variable(name="BR", value=-5),
+    #     "BB": Variable(name="BB", value=-3),
+    #     "BN": Variable(name="BN", value=-3),
+    #     "BG": Variable(name="BG", value=-2),  # Grasshopper (inverted queen)
+    #     "BP": Variable(name="BP", value=-1),
+    # }
 
     problem = Problem()
     # Create 8x8 variables
-    columns = list("abcdefgh")
-    rows = list(map(str, range(1, 9)))
+
     variables = ["".join(x) for x in product(columns, rows)]
 
     # Variables
     # pin some variables to a specific domain
-    # problem.addVariable("e1", [pieces["WK"]])
-    # problem.addVariable("g1", [pieces["BK"]])
-    # problem.addVariable("e2", [pieces["WP"]])
-    # problem.addVariable("h2", [pieces["BP"]])
-    # problem.addVariable("f3", [pieces["WB"]])
-    # problem.addVariable("h1", [pieces["WN"]])  # White Knight moved from g3 to h1
-    # problem.addVariable("g5", [pieces["WP"]])
-    # problem.addVariable("f7", [pieces["BP"]])
-    # problem.addVariable("g7", [pieces["WR"]])
+    problem.addVariable("e1", [pieces["WK"]])
+    problem.addVariable("g1", [pieces["BK"]])
+    problem.addVariable("e2", [pieces["WP"]])
+    problem.addVariable("h2", [pieces["BP"]])
+    problem.addVariable("f3", [pieces["WB"]])
+    problem.addVariable("h1", [pieces["WN"]])  # White Knight moved from g3 to h1
+    problem.addVariable("g5", [pieces["WP"]])
+    problem.addVariable("f7", [pieces["BP"]])
+    problem.addVariable("g7", [pieces["WR"]])
     # problem.addVariable("e1", [pieces_vars["WK"]])
     # problem.addVariable("g1", [pieces_vars["BK"]])
     # problem.addVariable("e2", [pieces_vars["WP"]])
@@ -176,7 +258,7 @@ def main():
 
     # fill all variables with default domain
     undeclared_vars = [var for var in variables if var not in problem._variables]
-    problem.addVariables(undeclared_vars, list(pieces_vars.values()))
+    problem.addVariables(undeclared_vars, list(pieces.values()))
 
     # Constraints
 
@@ -202,18 +284,57 @@ def main():
             ]
             problem.addConstraint(AllDifferentConstraint(), vars_in_quadrant)
 
-        # define cages (from bottomleft to topright, just like the ordering is with chess
-        # for maximum consistency
-        # cages = [
-        #     Cage(vars=["a1", "b1"], sum=-8),
-        # ]
-        # for cage in cages:
-        #     # problem.addConstraint(AllDifferentConstraint(), cage.vars)
-        #     problem.addConstraint(ExactSumConstraint(cage.sum), cage.vars)
+    # define cages (from bottomleft to topright, just like the ordering is with chess
+    # for maximum consistency
+    cages = [
+        Cage(vars=["a1", "b1"], sum=-8),
+        Cage(vars=["c2", "c1"], sum=11),
+        Cage(vars=["d2", "d1"], sum=-5),
+        Cage(vars=["e2", "e1", "f1", "g1", "h1", "f2", "g2", "h2"], sum=-4),
+        Cage(vars=["a3", "a2"], sum=9),
+        Cage(vars=["b3", "b2"], sum=0),
+        Cage(vars=["c3", "c4"], sum=0),
+        Cage(vars=["e3", "f3", "g3", "h3"], sum=-14),
+        # Cage(vars=["b4", "c4"], sum=0), # Difficult, might be because of the -3 +3 thing?
+        Cage(vars=["e4", "f4", "f5"], sum=13),
+        Cage(vars=["a5", "a4", "b5"], sum=-5),
+        Cage(vars=["g5", "g4", "h4", "h5"], sum=18),
+        Cage(vars=["a6", "b6"], sum=-1),
+        Cage(vars=["c6", "c5"], sum=-4),
+        # Cage(vars=["d6", "d5", "d4", "e6", "e5"], sum=-102), # Difficult
+        Cage(vars=["g6", "h6"], sum=-13),
+        Cage(vars=["b7", "c7", "d7"], sum=14),
+        Cage(vars=["e7", "f7", "f6"], sum=-1),
+        Cage(vars=["a8", "b8", "a7"], sum=92),
+        Cage(vars=["c8", "d8", "e8", "f8"], sum=0),
+        Cage(vars=["g8", "h8", "g7", "h7"], sum=0),
+    ]
+    for cage in cages:
+        problem.addConstraint(AllDifferentConstraint(), cage.vars)
+        problem.addConstraint(RoundedSumConstraint(cage.sum), cage.vars)
 
-    sol = problem.getSolution()
-    print(sol)
-    x = 1
+    for i, solution in enumerate(problem.getSolutionIter()):
+        if i > 10000:
+            break
+        export(solution, i, start_time, len(cages))
+
+
+def export(
+    solution: list[dict[str, int]], i: int, start_time: dt.datetime, n_cages: int
+):
+    now = start_time.strftime("%Y%m%d-%H%M%S")
+    folder = Path(f"export/{n_cages}/{now}")
+    folder.mkdir(parents=True, exist_ok=True)
+    path = folder / Path(f"{i}.sudoku")
+
+    string = ""
+    for row in reversed(rows):
+        for col in columns:
+            string += pieces_inv[solution[f"{col}{row}"]] + "\t"
+        string += "\n"
+        print(string)
+    with open(path, "w") as f:
+        f.write(string)
 
 
 if __name__ == "__main__":
